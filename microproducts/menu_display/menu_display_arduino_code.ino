edc7223fea621663ce0f83e3e5286c352ce0f7ec
nima34366 <wickramasinghenlssck@gmail.com>
2021-03-31 15:59:06 +0530

    printing any input through serial input

--- microproducts/menu_display/menu_display_arduino_code.ino
+++ microproducts/menu_display/menu_display_arduino_code.ino
@@ -3,11 +3,16 @@
 LiquidCrystal lcd(12,11,2,3,4,5);
 String a;
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.print("hello, world!");
 }
 
 void loop() {
-	
+  while(Serial.available()){
+  a= Serial.readString();
+  lcd.clear();
+  lcd.print(a);
+  }
 
 }
