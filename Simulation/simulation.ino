f4eb829cda67548a7a3f0fb347a040e6fd1e9b5d
Your Name <trajaka@gmail.com>
2021-04-02 12:32:14 +0530

    remove 1 for loop and change int(key) indexes

--- Simulation/simulation.ino
+++ Simulation/simulation.ino
@@ -4,9 +4,9 @@ const int ROW_NUM = 3; //four rows
 const int COLUMN_NUM = 3; //four columns
 
 char keys[ROW_NUM][COLUMN_NUM] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'}
 };
 
 byte pin_rows[ROW_NUM] = {A0, A1, A2}; //connect to the row pinouts of the keypad
@@ -16,31 +16,32 @@ Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_
 
 void setup()
 {
- for (int i=1; i<=9; i++) 
+ for (int i=2; i<=10; i++) 
  {
    pinMode(i, OUTPUT);
+   
  }
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  int key = keypad.getKey();
-  
- for (int i=1; i<=9; i++)
- {
-     if (key !=0 )
+  char key = keypad.getKey();
+ 
+     if (key != NO_KEY )
      {
-       for (int k = 1; k<=key; k++)
+       for (int k = 49; k<= (key); k++)
         {
-        digitalWrite(k,HIGH) ;
+        digitalWrite(k-47,HIGH) ;
+         Serial.println(int(key));
         }
+       delay(1000);
      } 
      else
      {
-       for (int p = 1; p<=9; p++)
+       for (int p = 2; p<=10; p++)
        {        
         digitalWrite(p,LOW);
        }
      }
- }
 }
