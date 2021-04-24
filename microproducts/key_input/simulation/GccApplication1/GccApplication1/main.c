2b923dcd3b290dcfc4a443da6715729e153e4a21
Thilanka1999 <trajaka@gmail.com>
2021-04-24 13:39:52 +0530

    add proteus file and atmel file

---
+++ microproducts/key_input/simulation/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2021-04-17 1:53:21 AM
+ * Author : Thilanka Rajaka
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
