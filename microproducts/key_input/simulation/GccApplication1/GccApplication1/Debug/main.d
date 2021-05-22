23707b04468f3c31c15f7d2663b6cb5928d0534a
Thilanka1999 <trajaka@gmail.com>
2021-05-22 23:22:15 +0530

    DDRC take out from the function

--- microproducts/key_input/simulation/GccApplication1/GccApplication1/Debug/main.d
+++ microproducts/key_input/simulation/GccApplication1/GccApplication1/Debug/main.d
@@ -10,7 +10,6 @@ main.d main.o: .././main.c \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
@@ -36,8 +35,6 @@ d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
 d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
-
-d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h:
 
 d:\atmel\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
--- microproducts/key_input/simulation/GccApplication1/GccApplication1/main.c
+++ microproducts/key_input/simulation/GccApplication1/GccApplication1/main.c
@@ -1,11 +1,13 @@
-#include <avr/io.h>
 #define F_CPU 8000000UL
-#include <avr/delay.h>
+#include <avr/io.h>
+#include <util/delay.h>
 int  key1, key2;
 void keypd();
 
 int main(void)
-{
+{	DDRB = 0b11111111; /* for light pins set to output */
+	DDRD = 0b10000000;
+	
 	while (1)
 	{
 		keypd();
@@ -15,17 +17,12 @@ int main(void)
 
 void keypd()
 {
-	DDRB = 0b11111111;; /* for light pins set to output */
-	DDRD = 0b10000000;;
-	
-	DDRC = 0b00111111;   /* pc0,pc1,pc2 set as output */
+	DDRC = 0b00000111;    /* pc0,pc1,pc2 set as output */	
 	PORTC = 0b00111000;   /* pc0-pc2 outputs turn on (HIGH)*/
-	
 	key2 = PINC & 0b00000111 ;
 	
-	DDRC = 0b00111111;    /* pc0,pc1,pc2 set as output */
+	DDRC = 0b00111000;    /* pc3,pc4,pc5 set as output */
 	PORTC = 0b00000111;   /* pc0-pc2 outputs turn on (HIGH)*/
-	
 	key1 = PINC & 0b00111000 ;
 	
 	if ((key1 == 0b00001000) & (key2 == 0b00000100)){PORTB = 0b10000000; }
@@ -39,7 +36,7 @@ void keypd()
 	if ((key1 == 0b00001000) & (key2 == 0b00000001)){PORTB = 0b11111110; }
 	if ((key1 == 0b00010000) & (key2 == 0b00000001)){PORTB = 0b11111111; }
 	if ((key1 == 0b00100000) & (key2 == 0b00000001)){PORTB = 0b11111111; PORTD = 0b10000000; }
-	_delay_ms(1000);
+	_delay_ms(100);
 	PORTB = 0x0;
 	PORTD = 0;
 }
