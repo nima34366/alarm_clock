e192a2ef4273f0c76e3d351c62d5c4c1ccd05085
Thilanka1999 <trajaka@gmail.com>
2021-06-01 00:26:23 +0530

    updated maun file for header file installation

--- microproducts/key_input/simulation/GccApplication1/GccApplication1/main.c
+++ microproducts/key_input/simulation/GccApplication1/GccApplication1/main.c
@@ -1,20 +1,11 @@
-#define F_CPU 16000000UL
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+
 #include <avr/io.h>
 #include <util/delay.h>
+#include "keypad.h"
 
-int keypd();
-
-int keypd(int *key1, int *key2)
-{
-	DDRC = 0b00000111;    /* pc0,pc1,pc2 set as output */
-	PORTC = 0b00111000;   /* pc0-pc2 outputs turn on (HIGH)*/
-	*key2 = PINC & 0b00000111 ;
-	
-	DDRC = 0b00111000;    /* pc3,pc4,pc5 set as output */
-	PORTC = 0b00000111;   /* pc0-pc2 outputs turn on (HIGH)*/
-	*key1 = PINC & 0b00111000 ;
-	return 0;
-}
 
 int main(void)
 {	DDRB = 0b11111111; /* for light pins set to output */
