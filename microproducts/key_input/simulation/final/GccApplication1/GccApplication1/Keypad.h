3f9c70336ce48169f301be32cd04e47f6082379b
Thilanka1999 <trajaka@gmail.com>
2021-06-08 02:22:57 +0530

    add menu read function

--- microproducts/key_input/simulation/final/GccApplication1/GccApplication1/Keypad.h
+++ microproducts/key_input/simulation/final/GccApplication1/GccApplication1/Keypad.h
@@ -12,37 +12,41 @@ int Keypad_read()
 {
 	data_diection_c = 0b11110000;    //DDRC 1 output, 0 input
 	port_state_c = 0b00001111;   //PORTC 1 high, 0 low
-	int row = PINC & 0b11110000 ; // check which row pressed
+	int colomn = PINC & 0b11110000 ; // check which colomn pressed
 
 	data_diection_c = 0b00001111;    
 	port_state_c = 0b11110000;   
-	int colomn = PINC & 0b00001111 ; // check which row pressed
+	int row = PINC & 0b00001111 ; // check which colomn pressed
 	int press;
 	
 	//row1 set to PC0 and so on, colom1 set to PC4 and so on//
 	
 	while (1)
 	{
-		if ((row == 0b00010000) & (colomn == 0b00000001)){press = 1; } 
-		if ((row == 0b00100000) & (colomn == 0b00000001)){press = 2; }
-		if ((row == 0b01000000) & (colomn == 0b00000001)){press = 3; }
-		if ((row == 0b10000000) & (colomn == 0b00000001)){press = 10;}
+		if ((colomn == 1<<4) & (row == 1)){press = 1; } 
+		if ((colomn == 1<<5) & (row == 1)){press = 2; }
+		if ((colomn == 1<<6) & (row == 1)){press = 3; }
+		if ((colomn == 1<<4) & (row == 1<<3)){press = 10;}
 		
-		if ((row == 0b00010000) & (colomn == 0b00000010)){press = 4; }
-		if ((row == 0b00100000) & (colomn == 0b00000010)){press = 5; }
-		if ((row == 0b01000000) & (colomn == 0b00000010)){press = 6; }
-		if ((row == 0b10000000) & (colomn == 0b00000010)){press = 20;}
+		if ((colomn == 1<<4) & (row == 1<<1)){press = 4; }
+		if ((colomn == 1<<5) & (row == 1<<1)){press = 5; }
+		if ((colomn == 1<<6) & (row == 1<<1)){press = 6; }
+		if ((colomn == 1<<6) & (row == 1<<3)){press = 20;}
 		
-		if ((row == 0b00010000) & (colomn == 0b00000100)){press = 7; }
-		if ((row == 0b00100000) & (colomn == 0b00000100)){press = 8; }
-		if ((row == 0b01000000) & (colomn == 0b00000100)){press = 9; }
-		if ((row == 0b10000000) & (colomn == 0b00000100)){press = 30;}
-
-		if ((row == 0b10000000) & (colomn == 0b00001000)){press = 40;}
+		if ((colomn == 1<<4) & (row == 1<<2)){press = 7; }
+		if ((colomn == 1<<5) & (row == 1<<2)){press = 8; }
+		if ((colomn == 1<<6) & (row == 1<<2)){press = 9; }
+		
 		else {return 0;}
 		_delay_ms(100);
 		return press;
 	}
+}
+int menu_read(){
+	data_diection_c |= 1<<7;
+	if (PINC==1<<7){return 1;} 
+	else {return 0;}
+	
 }
 
 #endif /* KEYPAD_H_ */
