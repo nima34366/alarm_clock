24bca155aee09962edb77ca40eab7d16ed5a8029
Dilani Widanapathiranage <81350898+DNWidanapathiranage@users.noreply.github.com>
2021-06-08 21:37:36 +0530

    fixed bugs

--- Source Code/audio_output.h
+++ Source Code/audio_output.h
@@ -16,13 +16,9 @@
 #include <stdlib.h>
 #include "notes.h"
 #include "tones.h"
-static int stop_audio;
-static int check_input_pin;
-static float Note_frequency;
-static float Note_duration;
 #define PORT PORTC
 #define PIN_SPEAKER_OUT PINC0  //speaker port
-#define PIN_STOP_ALARM PINC2
+#define PIN_STOP_ALARM PINC1
 
 class Tones
 {
@@ -32,10 +28,9 @@ class Tones
 	int *durations;
 	int length;
 	int rate;
-	inline void play_note(float Note_frequency,float Note_duration);
-	inline void audio_play();
+	void play_note(float Note_frequency,float Note_duration);
+	void audio_play();
 	
 };
 
 #endif
-
