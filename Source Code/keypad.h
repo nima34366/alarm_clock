41a469e4ea3020f5f7105ee32925f619b503d7a9
Thilanka1999 <trajaka@gmail.com>
2021-06-08 21:11:02 +0530

    add inline column and row define inside the while loop

--- Source Code/keypad.h
+++ Source Code/keypad.h
@@ -5,48 +5,42 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define  data_diection_c DDRC
-#define  port_state_c PORTC
+#define  data_diection_d DDRD
+#define  port_state_d PORTD
 
-int Keypad_read()
+inline int Keypad_read()
 {
-	data_diection_c = 0b11110000;    //DDRC 1 output, 0 input
-	port_state_c = 0b00001111;   //PORTC 1 high, 0 low
-	int colomn = PINC & 0b11110000 ; // check which colomn pressed
-
-	data_diection_c = 0b00001111;    
-	port_state_c = 0b11110000;   
-	int row = PINC & 0b00001111 ; // check which colomn pressed
-	int press;
-	
-	//row1 set to PC0 and so on, colom1 set to PC4 and so on//
-	
+	data_diection_d = 0b11111111;    //DDRC 1 output, 0 input	
 	while (1)
-	{
-		if ((colomn == 1<<4) & (row == 1)){press = 1; } 
-		if ((colomn == 1<<5) & (row == 1)){press = 2; }
-		if ((colomn == 1<<6) & (row == 1)){press = 3; }
-		if ((colomn == 1<<4) & (row == 1<<3)){press = 10;}
+	{	port_state_d = 0b00001111;   //PORTC 1 high, 0 low
+		int colomn = PIND & 0b11110000; // check which colomn pressed
 		
-		if ((colomn == 1<<4) & (row == 1<<1)){press = 4; }
-		if ((colomn == 1<<5) & (row == 1<<1)){press = 5; }
-		if ((colomn == 1<<6) & (row == 1<<1)){press = 6; }
-		if ((colomn == 1<<6) & (row == 1<<3)){press = 20;}
+		port_state_d = 0b11110000;
+		int row = PIND & 0b00001111; // check which colomn pressed
+		//row1 set to PC0 and so on, colom1 set to PC4 and so on//
 		
-		if ((colomn == 1<<4) & (row == 1<<2)){press = 7; }
-		if ((colomn == 1<<5) & (row == 1<<2)){press = 8; }
-		if ((colomn == 1<<6) & (row == 1<<2)){press = 9; }
+		if ((colomn == 1<<4) & (row == 1)){return 1; } 
+		if ((colomn == 1<<5) & (row == 1)){return 2; }
+		if ((colomn == 1<<6) & (row == 1)){return 3; }
+		if ((colomn == 1<<4) & (row == 1<<3)){return  10;}
 		
-		else {return 0;}
+		if ((colomn == 1<<4) & (row == 1<<1)){return  4; }
+		if ((colomn == 1<<5) & (row == 1<<1)){return  5; }
+		if ((colomn == 1<<6) & (row == 1<<1)){return  6; }
+		if ((colomn == 1<<6) & (row == 1<<3)){return  20;}
+		
+		if ((colomn == 1<<4) & (row == 1<<2)){return  7; }
+		if ((colomn == 1<<5) & (row == 1<<2)){return  8; }
+		if ((colomn == 1<<6) & (row == 1<<2)){return  9; }
+		if ((colomn == 1<<5) & (row == 1<<3)){return  0; }
 		_delay_ms(100);
-		return press;
 	}
 }
-int menu_read(){
-	data_diection_c |= 1<<7;
-	if (PINC==1<<7){return 1;} 
-	else {return 0;}
-	
+
+inline int menu_read(){
+	data_diection_d |= 1<<7;
+	if (PIND==1<<7){return 1;} 
+	else {return 0;}	
 }
 
 #endif /* KEYPAD_H_ */
